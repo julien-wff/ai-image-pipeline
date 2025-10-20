0ba50fc2b2cfe21e2510c14f83215f3c0baf1483
Julien W <cefadrom1@gmail.com>
2025-10-20 10:09:50 +0200

    chores: add shadcn svelte

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8000',
+            '/uploads': 'http://localhost:8000',
+            '/processed': 'http://localhost:8000',
+            '/docs': 'http://localhost:8000',
+            '/openapi.json': 'http://localhost:8000',
+        },
+    },
 });
